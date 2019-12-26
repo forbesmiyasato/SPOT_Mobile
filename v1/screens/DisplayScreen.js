1ce7030341cf796c7181590c4040b5d53c60807f
forbesmiyasato <miya5288@pacificu.edu>
2019-12-25 21:33:40 -0800

    Display Page now retrives necessary data from database, and passes data to listView to display

--- v1/screens/DisplayScreen.js
+++ v1/screens/DisplayScreen.js
@@ -1,26 +1,51 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import Axios from 'axios';
+import ListView from './ListView';
 
 const DisplayScreen = props => {
-    console.log(props.navigation.getParam('location'));
-    const [inputLocation, setInputLocation] = useState();
+    const [inputLocation, setInputLocation] = useState(props.navigation.getParam('location'));
     const [parkingLots, setParkingLots] = useState([]);
 
     useEffect(() => {
-       Axios.get('http://10.0.2.2:5000/ParkingLot/All')
+        var ParkingLots = [];
+        Axios.get('http://10.0.2.2:5000/ParkingLot/All')
             .then(response => {
-                console.log(response)
+                response.data.map((ParkingLot) => {
+                    var destination = `${ParkingLot.Lat}/${ParkingLot.Lng}`;
+                    var origin = `${inputLocation.lat}/${inputLocation.lng}`
+
+                    Axios.get(`http://10.0.2.2:5000/distancematrix/${origin}/${destination}`)
+                        .then(response => {
+                            ParkingLot["Distance"] = response.data.distance;
+                            ParkingLot["Duration"] = response.data.duration;
+                            ParkingLot["TimeUnit"] = response.data.unit;
+                            Axios.get(`http://10.0.2.2:5000/ParkingLot/${ParkingLot._id}/SnapShots/latest`)
+                                .then(response => {
+                                    ParkingLot["OpenParkings"] = response.data;
+                                })
+                                .then (()=> {
+                                    delete ParkingLot['SnapShots'];
+                                    ParkingLots.push(ParkingLot);
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                })
+                        })  
+                        .catch(error => {
+                            console.log(error);
+                        })
+                })
             })
-            .catch (error => {
+            .catch(error => {
                 console.log(error);
             })
 
+            setParkingLots(ParkingLots);
     }, [])
 
-
     return (
-        <Text>Test</Text>
+        <ListView data={parkingLots}/>
     )
 }
 
--- v1/screens/ListView.js
+++ v1/screens/ListView.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = props => {
-    console.log(props);
+    const renderGridItem = (itemData) => {
+        return (
+            <View key={itemData.item._id}>
+            <Text style={styles.Name}>{itemData.item.Name}</Text>
+            </View>
+        )
+    }
+
     return (
-        <Text></Text>
+        <View>
+        <FlatList data={props.data} renderItem={renderGridItem} numColumns={2} />
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    Name: {
+        color: 'black'
+    }
 });
 
 export default ListScreen;

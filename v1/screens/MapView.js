a9870d7f0868568c389fa58cfdcead18d24bdeb1
forbesmiyasato <miya5288@pacificu.edu>
2019-12-31 15:57:57 -0800

    added google map to map view

--- v1/screens/MapView.js
+++ v1/screens/MapView.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
-const MapView = (props) => {
+const MapViewWrapper = (props) => {
     return (
         <View>
-            <Text>MAP</Text>
+            <MapView
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                style={styles.mapStyle}
+            />
         </View>
     )
 }
 
-export default MapView;
+const styles = StyleSheet.create({
+    mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+      },
+});
+
+export default MapViewWrapper;
